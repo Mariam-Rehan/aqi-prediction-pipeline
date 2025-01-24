2632b6054cd1c5d557b877447fc9d18fe48ae7e4
Mariam-Rehan <97904555+Mariam-Rehan@users.noreply.github.com>
2025-01-24 06:27:36 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,8 +13,7 @@ st.title("Real-time AQI Forecast")
 #api_key = st.secrets["hopsworks"]["api_key"]
 #project = hopsworks.login(api_key_value=api_key)
 
-project = hopsworks.login(project = "aqi_prediction_mariam", api_key_value = "fGaRronKJ6ZMI6K0.4iXEy9yDd6VkxOypTtfseQQ1Ip3a9sREDzgx6Qnezj50mhqfD7DrzfBlpQPMFAuM")
-streamlit run app.py "fGaRronKJ6ZMI6K0.4iXEy9yDd6VkxOypTtfseQQ1Ip3a9sREDzgx6Qnezj50mhqfD7DrzfBlpQPMFAuM"
+project = hopsworks.login()
 fs = project.get_feature_store()
 feature_group = fs.get_or_create_feature_group(
     name="aqi_weather_features",
